ef43cb1742d0a73a185e28bb1ee829f602693c32
Amy <agomezm15@est.ups.edu.ec>
2025-10-31 17:18:41 -0500

    Clases editadas por nuevas clases creadas (Actor, Temporada, etc)

--- src/uni1a/Documental.java
+++ src/uni1a/Documental.java
@@ -6,10 +6,12 @@ package uni1a;
 // Subclase Documental que extiende de ContenidoAudiovisual
 public class Documental extends ContenidoAudiovisual {
     private String tema;
-
-    public Documental(String titulo, int duracionEnMinutos, String genero, String tema) {
+    private Investigador investigadorPrincipal; //Agregación: Un Documental tiene un Investigador principal
+    
+    public Documental(String titulo, int duracionEnMinutos, String genero, String tema, Investigador investigadorPrincipal) {
         super(titulo, duracionEnMinutos, genero);
         this.tema = tema;
+        this.investigadorPrincipal= investigadorPrincipal; //Asignar el objeto Investigador 
     }
 
     public String getTema() {
@@ -19,15 +21,25 @@ public class Documental extends ContenidoAudiovisual {
     public void setTema(String tema) {
         this.tema = tema;
     }
+    public Investigador getInvestigadorPrincipal() { //Nuevo get
+    	return investigadorPrincipal;
+    }
+    public void setInvestigadorPrincipal (Investigador investigadorPrincipal) {
+    	this.investigadorPrincipal= investigadorPrincipal;
+    }
     
     @Override
     public void mostrarDetalles() {
-        System.out.println("Detalles de la película:");
+        System.out.println("Detalles del documental:");
         System.out.println("ID: " + getId());
         System.out.println("Título: " + getTitulo());
         System.out.println("Duración en minutos: " + getDuracionEnMinutos());
         System.out.println("Género: " + getGenero());
         System.out.println("Tema: " + this.tema);
+        //Mostar detalles del investigador
+        if (investigadorPrincipal != null) {
+        	System.out.println(investigadorPrincipal.toString());
+        }
         System.out.println();
     }
 }
--- src/uni1a/Pelicula.java
+++ src/uni1a/Pelicula.java
@@ -6,12 +6,14 @@ package uni1a;
 // Subclase Pelicula que extiende de ContenidoAudiovisual
 public class Pelicula extends ContenidoAudiovisual {
     private String estudio;
+    private Actor actorPrincipal; 
 
-    public Pelicula(String titulo, int duracionEnMinutos, String genero, String estudio) {
+    public Pelicula(String titulo, int duracionEnMinutos, String genero, String estudio, Actor actorPrincipal) {
         super(titulo, duracionEnMinutos, genero);
         this.estudio = estudio;
+        this.actorPrincipal= actorPrincipal; //Asignar el objeto Actor
     }
-
+    //Gtters y Setters originales
     public String getEstudio() {
         return estudio;
     }
@@ -20,6 +22,14 @@ public class Pelicula extends ContenidoAudiovisual {
         this.estudio = estudio;
     }
     
+    //Nuevos getters y setters para el Actor 
+    public Actor getActorPrincipal () {
+    	return actorPrincipal;
+    }
+    public void setActorPrincipal (Actor actorPrincipal) {
+    	this.actorPrincipal= actorPrincipal;
+    }
+    //Metodo modificado (para que ahora tb muestre los detalles del actor)
     @Override
     public void mostrarDetalles() {
         System.out.println("Detalles de la película:");
@@ -28,6 +38,10 @@ public class Pelicula extends ContenidoAudiovisual {
         System.out.println("Duración en minutos: " + getDuracionEnMinutos());
         System.out.println("Género: " + getGenero());
         System.out.println("Estudio: " + estudio);
+        //Mostrar detalles del auor 
+        if (actorPrincipal !=null) {
+        	System.out.println(actorPrincipal.toString());	
+        }
         System.out.println();
     }
 }
